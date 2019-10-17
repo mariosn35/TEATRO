bc7fb8085aec173cc7ed26c125e416001209609e
mariosn35 <mariosn35@hotmail.com>
2019-10-17 19:32:46 +0200

    Cambio Usuario

--- Teatro/src/modelos/Usuario.java
+++ Teatro/src/modelos/Usuario.java
@@ -7,18 +7,30 @@ package modelos;
  */
 public class Usuario {
     private long identificador;/*Modela id. Valor positivo*/
+    private String nombre;/*Modela nombre. Contiene solo caracteres alfabéticos*/
+    private String telefono;/*Modela id. Valor positivo*/
+    private String NIF;/*Modela id. Valor positivo*/
+    private String email;/*Modela id. Valor positivo. Contiene números, letras y caracteres especiales.*/
 
     public Usuario() {
     }
     
-    public Usuario(long identificador) {
+    public Usuario(long identificador,String nombre, String telefono, String NIF, String email) {
        this.identificador = identificador;
+       this.nombre = nombre;
+       this.telefono = telefono;
+       this.NIF = NIF;
+       this.email = email;
     }
     
     public Usuario (Usuario u) {
        this.identificador = u.getIdentificador();
+       this.nombre = u.getNombre();
+       this.telefono = u.getTelefono();
+       this.NIF = u.getNIF();
+       this.email = u.getEmail();      
     }
-    
+
     public long getIdentificador() {
         return identificador;
     }
@@ -26,14 +38,46 @@ public class Usuario {
     public void setIdentificador(long identificador) {
         this.identificador = identificador;
     }
-    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNIF() {
+        return NIF;
+    }
+
+    public void setNIF(String NIF) {
+        this.NIF = NIF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
-        return "Usuario{" + "identificador=" + identificador + '}';
+        return "Usuario{" + "identificador=" + identificador + ", nombre=" + nombre + ", telefono=" + telefono + ", NIF=" + NIF + ", email=" + email + '}';
     }
     
     public String data() {
-        return ""+getIdentificador();
+        return ""+getIdentificador()+"|"+getNombre()+"|"+getTelefono()+"|"+getNIF()+"|"+getEmail();
     }
     
 }

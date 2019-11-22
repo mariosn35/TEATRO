9d9b3bfdb75c53a3b8befcac68cceb171a1bd00b
AidanLL <aidanyoyo01@gmail.com>
2019-11-22 17:00:36 +0100

    cambio LBB

--- Teatro/src/modelos/EOficina.java
+++ Teatro/src/modelos/EOficina.java
@@ -1,13 +1,17 @@
 package modelos;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 /**
  *
- * @author roterr
+ * @author roter
  * @version 1.0
  */
 public class EOficina extends Empleado {
@@ -34,6 +38,48 @@ public class EOficina extends Empleado {
 
     }
 
+
+         public static EOficina nuevoEOficina() {
+        
+        Scanner in;
+        int a ;
+        EOficina o = new EOficina();
+        in = new Scanner(System.in);
+        System.out.println("Mete tus años de antiguedad");
+        a=in.nextInt();
+        o.getAntiguedad();
+        o= (EOficina) Empleado.nuevoEmpleado();
+    
+    return o;
+    
+       }
+           public EOficina getEOficinaById (long Id) {
+    EOficina e = null;
+    /* for(int i=0; i< Lista.size (); i++){
+     e=(Empleado) Lista[i];
+     
+        if( e.getId()== Id){
+        }
+        
+     } 
+     */
+    return e;
+    }
+    
+    public ArrayList<EOficina> getAllEOficinas() {
+       ArrayList<EOficina> EOficinas= new ArrayList();
+       /*for (int i=0; i< Lista.size(); i++)
+           e= Empleado Lista[i];
+       Empleados.add(f);
+       
+       */
+        return EOficinas;
+    }
+
+    
+
+
+
     public int getAntiguedad() {
         return antiguedad;
     }
@@ -47,6 +93,7 @@ public class EOficina extends Empleado {
         return super.toString() + "EOficina{" + "antiguedad=" + antiguedad + '}';
     }
 
+    @Override
     public String data() {
         return super.data() + "" + getAntiguedad();
     }

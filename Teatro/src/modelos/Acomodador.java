5f1dc5c170033f86f76bed87bb3f856fb976245c
mariosn35 <mariosn35@hotmail.com>
2020-01-29 16:56:14 +0100

    Commit Mario

--- Teatro/src/modelos/Acomodador.java
+++ Teatro/src/modelos/Acomodador.java
@@ -1,9 +1,9 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. jajaja
  */
-package modelos;
+package modelos; 
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
